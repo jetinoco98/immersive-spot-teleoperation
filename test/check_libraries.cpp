fec0a4d3fc9fd2a236d849685b1eadea8075034f
Josue Tinoco <jetinoco98@hotmail.com>
2025-04-28 12:44:56 +0200

    Minor changes to CMakeLists and testing files

--- test/check_libraries.cpp
+++ test/check_libraries.cpp
@@ -25,19 +25,31 @@
 
 
 int zmq_test() {
-    zmq::context_t context(1);
-    zmq::socket_t publisher(context, zmq::socket_type::pub);
-    publisher.bind("tcp://*:5555");
+    float data[] = { 0.0f, -2.0f, -3.0f, 4.0f, 5.0f, 6.0f };
 
-    int message_number = 0;
+    zmq::context_t zmq_context(1);
+    zmq::socket_t publisher(zmq_context, zmq::socket_type::pub);
+    publisher.bind("tcp://*:5555"); 
+    std::string topic = "from_cpp";  
+
     while (true) {
-        std::string message = "Hello from C++ Publisher: " + std::to_string(message_number++);
-        zmq::message_t zmq_message(message.begin(), message.end());
-        publisher.send(zmq_message, zmq::send_flags::none);
-        std::cout << "Sent: " << message << std::endl;
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
+        zmq::message_t topic_msg(topic.data(), topic.size());
+        publisher.send(topic_msg, zmq::send_flags::sndmore);
 
+        // Increment first value
+        data[0] += 1.0f;
+
+        zmq::message_t data_msg(sizeof(data)); 
+        std::memcpy(data_msg.data(), data, sizeof(data));
+        publisher.send(data_msg, zmq::send_flags::none);
+
+        printf("Sent data: %.1f, %.1f, %.1f, %.1f, %.1f, %.1f\n", 
+               data[0], data[1], data[2], data[3], data[4], data[5]);
+
+        // Send every second
+        std::this_thread::sleep_for(std::chrono::seconds(1));  
+    }
+    
     return 0;
 }
 
